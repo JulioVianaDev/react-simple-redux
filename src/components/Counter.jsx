d1bfd8b36c8456beb0109ba1b3e92f5ac37f5ba5
juliovianadev <juliovianadev@gmail.com>
2023-02-28 15:06:16 -0300

    agora adicionando por 5

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,13 +11,16 @@ const Counter = () => {
   const DecrementHandler = () => {
     dispatch({type: 'decrement'})
   };
-
+  const IncreaseHandler = () => {
+    dispatch({type: 'increase',amount: 5 })
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={IncrementHandler}> Increment</button>
+        <button onClick={IncreaseHandler}> Increase by 5</button>
         <button onClick={DecrementHandler}> Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
