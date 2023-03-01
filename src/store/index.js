dc47d563070acb457c8b62adebdecec4ad7f274b
juliovianadev <juliovianadev@gmail.com>
2023-02-28 22:30:57 -0300

    reducer toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
 import { createStore } from "redux";
-
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {counter: 0,showCounter: false }
 
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state){
+      state.counter++;
+    },
+    decrement(state){
+      state.counter--;
+    },
+    increase(state,action){
+      state.counter = state.counter + action.amount
+    },
+    toogleCounter(state){
+      state.showCounter = !state.showCounter
+    }
+  }
 
+})
 export const INCREMENT = 'increment'
 
 const counterReducer = (state = initialState ,action)=>{
